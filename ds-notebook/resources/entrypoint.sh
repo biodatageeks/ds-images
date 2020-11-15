ad8f38d9ea7366fe1cd62f71ac8bcf58e362c804
mwiewior <marek.wiewiorka@gmail.com>
2020-11-15 08:02:37 +0100

    Adding GCS setup

--- ds-notebook/resources/entrypoint.sh
+++ ds-notebook/resources/entrypoint.sh
@@ -7,6 +7,14 @@ echo "$@"
 #prepare repos
 BIODATAGEEKS_REPOS=${BIODATAGEEKS_REPOS:-"https://oss.sonatype.org/content/repositories/snapshots/"}
 
+#save GCS key
+: "${DS_LAB_GCS_KEY:?GCS key is missing!}"
+
+mkdir -p $HOME/secrets && echo $DS_LAB_GCS_KEY > $HOME/secrets/$SERVICE_ACCOUNT.json
+export GOOGLE_APPLICATION_CREDENTIALS=$HOME/secrets/$SERVICE_ACCOUNT.json
+export PROJECT_ID=$(cat $GOOGLE_APPLICATION_CREDENTIALS | jq '.project_id' | sed 's/"//g')
+
+envsubst < /tmp/.boto_template > $HOME/.boto
 
 export PYSPARK_SUBMIT_ARGS="--repositories ${BIODATAGEEKS_REPOS}  pyspark-shell"
 
@@ -15,6 +23,8 @@ mkdir -p $HOME/work/git
 cd $HOME/work/git
 git clone $NOTEBOOKS_REPO
 cd $HOME
+
+
 
 tini -g -- "$@"
 
