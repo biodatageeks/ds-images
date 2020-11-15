ab9d21dcac602dcd02a6f99b4d0ba9f923e3ec53
mwiewior <marek.wiewiorka@gmail.com>
2020-11-15 13:02:31 +0100

    Spark distributed on GKE conf

--- ds-notebook/resources/entrypoint.sh
+++ ds-notebook/resources/entrypoint.sh
@@ -10,19 +10,35 @@ BIODATAGEEKS_REPOS=${BIODATAGEEKS_REPOS:-"https://oss.sonatype.org/content/repos
 #save GCS key
 : "${DS_LAB_GCS_KEY:?GCS key is missing!}"
 
-mkdir -p $HOME/secrets && echo $DS_LAB_GCS_KEY > $HOME/secrets/$SERVICE_ACCOUNT.json
-export GOOGLE_APPLICATION_CREDENTIALS=$HOME/secrets/$SERVICE_ACCOUNT.json
+SECRETS_MOUNT_DIR=/tmp/secrets
+mkdir -p $SECRETS_MOUNT_DIR && echo $DS_LAB_GCS_KEY > $SECRETS_MOUNT_DIR/$SERVICE_ACCOUNT.json
+export GOOGLE_APPLICATION_CREDENTIALS=$SECRETS_MOUNT_DIR/$SERVICE_ACCOUNT.json
 export PROJECT_ID=$(cat $GOOGLE_APPLICATION_CREDENTIALS | jq '.project_id' | sed 's/"//g')
 
 envsubst < /tmp/.boto_template > $HOME/.boto
 
 export PYSPARK_SUBMIT_ARGS="--repositories ${BIODATAGEEKS_REPOS} \
-  --jars /tmp/gcs-connector-hadoop2-latest.jar
+  --jars /tmp/gcs-connector-hadoop2-latest.jar \
   --conf spark.hadoop.google.cloud.auth.service.account.enable=true \
   --conf spark.hadoop.google.cloud.auth.service.account.json.keyfile=$GOOGLE_APPLICATION_CREDENTIALS \
+  --conf spark.kubernetes.driverEnv.GCS_PROJECT_ID=$PROJECT_ID \
+  --conf spark.kubernetes.driverEnv.GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
+  --conf spark.executorEnv.GCS_PROJECT_ID=$PROJECT_ID \
+  --conf spark.executorEnv.GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
+  --conf spark.kubernetes.driver.secrets.$SERVICE_ACCOUNT-secret=$SECRETS_MOUNT_DIR \
+  --conf spark.kubernetes.executor.secrets.$SERVICE_ACCOUNT-secret=$SECRETS_MOUNT_DIR \
   --conf spark.hadoop.fs.gs.project.id=$PROJECT_ID \
   --conf spark.hadoop.fs.gs.impl=com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem \
-  --conf spark.hadoop.fs.AbstractFileSystem.gs.impl=com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS
+  --conf spark.hadoop.fs.AbstractFileSystem.gs.impl=com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS \
+  --master k8s://https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT \
+  --conf spark.kubernetes.container.image=$SPARK_IMAGE \
+  --conf spark.kubernetes.authenticate.driver.serviceAccountName=$SERVICE_ACCOUNT \
+  --conf spark.kubernetes.authenticate.serviceAccountName=$SERVICE_ACCOUNT \
+  --conf spark.driver.port=29010 \
+  --conf spark.blockManager.port=29011 \
+  --conf spark.kubernetes.namespace=default \
+  --conf spark.driver.host=jupyter-service-$JUPYTERHUB_USER \
+  --conf spark.driver.bindAddress=$HOSTNAME \
    pyspark-shell"
 
 #prepare notebooks
