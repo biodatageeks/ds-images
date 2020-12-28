107a2ca05488be597286e1cfedac7b191e856a9d
mwiewior <marek.wiewiorka@gmail.com>
2020-12-28 18:26:38 +0100

    Absoulute url to false

--- ds-notebook/resources/bin/start-airflow.sh
+++ ds-notebook/resources/bin/start-airflow.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 JUPYTERLAB_BASE_URL=${1}
-AIRFLOW_PORT=${2}
+AIRFLOW_PORT=6000
 
 set -x
 
@@ -21,12 +21,81 @@ if [ ! -d ${AIRFLOW_HOME} ]; then
 fi
 
 
+# webserver script
+PYTHON_SCRIPT=`cat <<EOF
+import sys
+from http.server import BaseHTTPRequestHandler,HTTPServer
+
+# html page returned by GET requests
+html_disclaimer = """
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Airflow</title>
+	</head>
+	<body>
+		<h1>Info</h1>
+		<p>
+			Your Airflow is starting up please press the button to continue...
+		<p>
+		<form method="POST">
+			<input type="submit" value="OK" />
+		</form>
+	</body>
+</html>
+""".encode('utf-8')
+
+# html page returned by POST requests
+html_refresh = """
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirect - Airflow</title>
+		<meta http-equiv='refresh' content='10'>
+	</head>
+	<body>
+		<h1>Redirecting to Airflow...</h1>
+		<p>
+			You will be shortly redirected to Airflow. If the redirect doesn't work, please go back to JupyterLab and open Airflow again.
+		<p>
+	</body>
+</html>
+""".encode('utf-8')
+
+info_shown=False
+
+class DummyHandler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		self.send_response(200)
+		self.send_header('Content-type','text/html')
+		self.end_headers()
+		self.wfile.write(html_disclaimer)
+		return
+	def do_POST(self):
+		self.send_response(200)
+		self.send_header('Content-type','text/html')
+		self.end_headers()
+		self.wfile.write(html_refresh)
+		global info_shown
+		info_shown=True
+		return
+
+server = HTTPServer(('', ${AIRFLOW_PORT}), DummyHandler)
+
+while not info_shown:
+	server.handle_request()
+EOF`
+
+python -c "$PYTHON_SCRIPT"
+
+
 # set the base url for airflow webserver
 export AIRFLOW__WEBSERVER__BASE_URL="https://${LAB_DOMAIN}/user/${USER}/airflow"
+export AIRFLOW__WEBSERVER__ENABLE_PROXY_FIX="True"
 #load examples
 export AIRFLOW__CORE__LOAD_EXAMPLES="True"
 
-airflow webserver --port 6000 &
+airflow webserver --port $AIRFLOW_PORT &
 airflow scheduler &
 
 wait
