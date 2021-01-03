88dd5a7a340aafe7c7df5bb8e7d63dc8a48326f6
mwiewior <marek.wiewiorka@gmail.com>
2021-01-03 20:20:09 +0100

    Fixing mlflow db upgrade

--- ds-notebook/resources/bin/start-mlflow.sh
+++ ds-notebook/resources/bin/start-mlflow.sh
@@ -10,7 +10,8 @@ mkdir -p $MFLOW_HOME/experiments
 
 source /opt/conda/etc/profile.d/conda.sh
 conda activate $HOME/venv/$JUPYTER_KERNEL_NAME
-mlflow db upgrade
+
+mlflow db upgrade sqlite:///${MFLOW_HOME}/experiments/mlflow.db
 mlflow server --host 0.0.0.0 --port $MLFLOW_UI_PORT \
 --default-artifact-root gs://bdg-lab-${USER}/mlflow/artifacts \
 --backend-store-uri sqlite:///${MFLOW_HOME}/experiments/mlflow.db &
