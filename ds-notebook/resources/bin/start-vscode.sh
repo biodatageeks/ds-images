d8082aff58d6b597803766bebfb07f75cbd9110a
mwiewior <marek.wiewiorka@gmail.com>
2021-06-04 16:03:31 +0200

    Fixing vs-code permissions

--- ds-notebook/resources/bin/start-vscode.sh
+++ ds-notebook/resources/bin/start-vscode.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+/opt/code-server-*-linux-amd64/bin/code-server --install-extension ms-python.python
 /opt/code-server-*-linux-amd64/bin/code-server --auth none --bind-addr 0.0.0.0:7000 --disable-update-check --disable-telemetry -vvv
